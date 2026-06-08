866070f2d6b32056aa290ff4aff7e7a9c4ec55b7
MrSufferer <tkien2703@gmail.com>
2026-06-08 20:12:34 +0700

    add testcases

--- discount.py
+++ discount.py
@@ -1,4 +1,8 @@
-def calculate_discount(amount):
-    if amount >= 50000000:
+def calculate_discount(current_total, order_amount):
+    if current_total >= 50000000:
         return 0.1
+
+    if current_total + order_amount >= 50000000:
+        return 0.1
+
     return 0
--- test_discount.py
+++ test_discount.py
@@ -2,8 +2,20 @@ from discount import calculate_discount
 
 def test_vip_customer():
 
-    assert(calculate_discount(60000000) == 90)
+    assert(calculate_discount(60000000) == 0.1)
 
 def test_normal_customer():
 
-    assert(calculate_discount(300000000) == 10)
+    assert(calculate_discount(30000000) == 0.1)
+
+def test_become_vip_after_order():
+    assert calculate_discount(49000000, 2000000) == 0.1
+
+def test_reach_exactly_50m():
+    assert calculate_discount(48000000, 2000000) == 0.1
+
+def test_still_not_vip():
+    assert calculate_discount(30000000, 2000000) == 0
+
+def test_already_vip():
+    assert calculate_discount(60000000, 2000000) == 0.1
